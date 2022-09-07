5223d2d0f5a01b98ff602869df94a4e03841f1f9
Edison <edisoncorderop@gmail.com>
2022-09-07 01:07:48 -0400

    add function remove book and add validations

--- function.js
+++ function.js
@@ -4,10 +4,11 @@ const pagesDom = document.querySelector('#pages')
 const readDom = document.querySelector('#read')
 const save = document.querySelector('.save')
 const container = document.querySelector('.container');
-
+let id = 1 ;
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
+   this.id = id++
    this.title = title; 
    this.author = author;
    this.pages = pages;
@@ -36,25 +37,55 @@ save.addEventListener('click', ()=> {
    
 })
 
-function cardRender() {
+ 
 
-    const div = document.createElement('div');
-    const title = document.createElement('h3'); 
-    const spanAuthor = document.createElement('span');
-    const spanPages = document.createElement('span');
-    const spanRead = document.createElement('span');
-    const bookTemp = myLibrary[myLibrary.length - 1];
-    div.classList.add('book');
-    title.innerHTML =  bookTemp.title;
-    spanAuthor.innerHTML = "By: " + bookTemp.author;
-    spanPages.innerHTML = "Number of Pages: " +  bookTemp.pages;
-    const read = bookTemp.read == 1 ? 'yes' : 'no' ;
-    spanRead.innerHTML = "Already Read: " + read;
-    div.appendChild(title); 
-    div.appendChild(spanAuthor); 
-    div.appendChild(spanPages); 
-    div.appendChild(spanRead); 
-    container.appendChild(div);
+
+function cardRender() {
+    container.innerHTML = "";
+    myLibrary.forEach(bookTemp => {
+        
+        
+        const div = document.createElement('div');
+        const title = document.createElement('h3'); 
+        const button = document.createElement('button');
+        const spanAuthor = document.createElement('span');
+        const spanPages = document.createElement('span');
+        const spanRead = document.createElement('span');
+        let read;
+        div.classList.add('book');
+        
+        /*const bookTemp = myLibrary[myLibrary.length - 1];*/
+    
+    
+        title.innerHTML =  bookTemp.title;
+        spanAuthor.innerHTML = "By: " + bookTemp.author;
+        spanPages.innerHTML = "Number of Pages: " +  bookTemp.pages;
+        if ( bookTemp.read == 1 ) {
+            read =  'yes' ;
+            div.classList.add('read')
+        } else {
+            read =  'no' ;
+            div.classList.add('notread')
+        }
+        spanRead.innerHTML = "Already Read: " + read;
+        button.innerHTML = "REMOVE"
+        button.classList.add('bookButton')
+        button.setAttribute('data-id', bookTemp.id );  
+        button.addEventListener('click', ()=> {   
+            let text = "Are you sure you want to remove the book.";
+            if (confirm(text) == true) {
+                removeBook(parseInt(button.getAttribute("data-id")));
+            }             
+        })
+        div.appendChild(title); 
+        div.appendChild(spanAuthor); 
+        div.appendChild(spanPages); 
+        div.appendChild(spanRead); 
+        div.appendChild(button); 
+        
+        container.appendChild(div);
+    });
+     
 }
 
 function validate (book) {
@@ -70,3 +101,10 @@ function validate (book) {
     else 
         return true;
 }
+
+function removeBook(bookId) {
+    const objWithIdIndex = myLibrary.findIndex(obj => obj.id === bookId);
+    myLibrary.splice( objWithIdIndex, 1 );
+    cardRender()
+     
+  }
